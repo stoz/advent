6e47bbd5aa2030d0e24437de47cbe07f18d55853
stoz <804461+stoz@users.noreply.github.com>
2022-12-11 15:57:52 +1100

    2022 Day 10

--- main.go
+++ main.go
@@ -10,7 +10,63 @@ import (
 )
 
 func main() {
-	rope2()
+	crt()
+}
+
+func crt2() {
+	// expected result: BJFRHRFU
+	lines := ReadFile("crt.txt")
+	x := 1
+	width := 40
+	output := ""
+	for _, line := range lines {
+		f := strings.Fields(line)
+		cycles := 1
+		add := 0
+		if f[0] == "addx" {
+			cycles = 2
+			add, _ = strconv.Atoi(f[1])
+		}
+		for i := 0; i < cycles; i++ {
+			c := len(output)
+			if c == x-1 || c == x || c == x+1 {
+				output += "#"
+			} else {
+				output += "."
+			}
+			if len(output) == width {
+				fmt.Println(output)
+				output = ""
+			}
+		}
+		x += add
+	}
+}
+
+func crt() {
+	// expected result: 14620
+	lines := ReadFile("crt.txt")
+	x := 1
+	c := 0
+	interesting := map[int]struct{}{20: {}, 60: {}, 100: {}, 140: {}, 180: {}, 220: {}}
+	sum := 0
+	for _, line := range lines {
+		f := strings.Fields(line)
+		cycles := 1
+		add := 0
+		if f[0] == "addx" {
+			cycles = 2
+			add, _ = strconv.Atoi(f[1])
+		}
+		for i := 0; i < cycles; i++ {
+			c++
+			if _, ok := interesting[c]; ok {
+				sum += x * c
+			}
+		}
+		x += add
+	}
+	fmt.Println(sum)
 }
 
 func rope2() {
