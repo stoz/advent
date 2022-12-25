fccdcdef08e0c0e570a20efcbf723594a1d51a3e
stoz <804461+stoz@users.noreply.github.com>
2022-12-25 12:57:37 +1100

    2022 Day 15 Cleanup

--- 2022-15.go
+++ 2022-15.go
@@ -2,14 +2,84 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-func xyz3() {
-	// 4350538 too low
-	// 4352518 too low
-	// 4370518 too low
-	lines := ReadFile("input.txt")
+func s22151(filename string, debug bool) int {
+	// expected result: 4907780
+	lines := ReadFile("./data/2022/15/" + filename)
+	var dim [4]int
+	cave := make(map[int]map[int]int)
+	sens := make(map[int]map[int]int)
+	for _, line := range lines {
+		ints := ExtractSInts(line)
+		for i, j := range ints {
+			switch i % 2 {
+			case 0: //x
+				if j < dim[2] || dim[2] == 0 {
+					dim[2] = j - 10000000
+				} else if j > dim[3] {
+					dim[3] = j + 10000000
+				}
+			case 1: //y
+				if j < dim[0] || dim[0] == 0 {
+					dim[0] = j - 10
+				} else if j > dim[1] {
+					dim[1] = j + 10
+				}
+			}
+		}
+	}
+	for i := dim[0]; i <= dim[1]; i++ {
+		cave[i] = make(map[int]int)
+	}
+	for _, line := range lines {
+		ints := ExtractInts(line)
+		var x int
+		var sensor [2]int
+		for i, y := range ints {
+			switch i % 4 {
+			case 0, 2:
+				x = y
+			case 1:
+				cave[y][x] = 3
+				sensor[0] = y
+				sensor[1] = x
+			case 3:
+				cave[y][x] = 4
+				_, ok := sens[sensor[0]]
+				if !ok {
+					sens[sensor[0]] = make(map[int]int)
+				}
+				sens[sensor[0]][sensor[1]] = abs(sensor[0]-y) + abs(sensor[1]-x)
+			}
+		}
+	}
+	if debug {
+		fmt.Println(dim)
+		PrintCave2(cave, dim)
+	}
+	var sum int
+	y := 2000000
+	for x := dim[2]; x <= dim[3]; x++ {
+		value, ok := cave[y][x]
+		safe := 0
+		if !ok {
+			// square is empty
+			// check if any sensors are in range
+			safe = InRangeOfSensor(y, x, sens)
+		} else {
+			if value == 3 {
+				safe = 1
+			}
+		}
+		sum += safe
+	}
+	return sum
+}
+
+func s22152(filename string, debug bool) int {
+	// expected result: 13639962836448
+	lines := ReadFile("./data/2022/15/" + filename)
 	var dim [4]int
 	cave := make(map[int]map[int]int)
 	sens := make(map[int]map[int]int)
@@ -68,7 +138,9 @@ func xyz3() {
 			}
 		}
 	}
-	fmt.Println(dim)
+	if debug {
+		fmt.Println(dim)
+	}
 	// 4000000
 	var lim = 4000000
 	var fin []int
@@ -108,8 +180,7 @@ func xyz3() {
 									safe := InRangeOfSensor(y, x, sens)
 									cache[x*lim+y] = true
 									if safe == 0 {
-										fin = append(fin, y, x)
-										fmt.Println("!!!", x*lim+y)
+										return x*lim + y
 									}
 								}
 							}
@@ -121,13 +192,16 @@ func xyz3() {
 		if len(fin) > 2 {
 			fmt.Println("too many", fin)
 			fmt.Println("---", fin[1]*lim+fin[0])
-			os.Exit(1)
+			return -1
 		}
 	}
-	fmt.Println(fin)
-	if len(fin) == 2 {
-		fmt.Print(fin[1]*lim + fin[0])
+	if debug {
+		fmt.Println(fin)
+		if len(fin) == 2 {
+			fmt.Print(fin[1]*lim + fin[0])
+		}
 	}
+	return -1
 }
 
 func InRangeOfSensor(y, x int, sens map[int]map[int]int) int {
@@ -139,147 +213,4 @@ func InRangeOfSensor(y, x int, sens map[int]map[int]int) int {
 		}
 	}
 	return 0
-}
-
-func xyz2() {
-	// 4350538 too low
-	// 4352518 too low
-	// 4370518 too low
-	lines := ReadFile("input.txt")
-	var dim [4]int
-	cave := make(map[int]map[int]int)
-	sens := make(map[int]map[int]int)
-	for _, line := range lines {
-		ints := ExtractInts(line)
-		for i, j := range ints {
-			switch i % 2 {
-			case 0: //x
-				if j < dim[2] || dim[2] == 0 {
-					dim[2] = j - 10000000
-				} else if j > dim[3] {
-					dim[3] = j + 10000000
-				}
-			case 1: //y
-				if j < dim[0] || dim[0] == 0 {
-					dim[0] = j - 10
-				} else if j > dim[1] {
-					dim[1] = j + 10
-				}
-			}
-		}
-	}
-	for i := dim[0]; i <= dim[1]; i++ {
-		cave[i] = make(map[int]int)
-	}
-	for _, line := range lines {
-		ints := ExtractInts(line)
-		var x int
-		var sensor [2]int
-		for i, y := range ints {
-			switch i % 4 {
-			case 0, 2:
-				x = y
-			case 1:
-				cave[y][x] = 3
-				sensor[0] = y
-				sensor[1] = x
-			case 3:
-				cave[y][x] = 4
-				_, ok := sens[sensor[0]]
-				if !ok {
-					sens[sensor[0]] = make(map[int]int)
-				}
-				sens[sensor[0]][sensor[1]] = abs(sensor[0]-y) + abs(sensor[1]-x)
-			}
-		}
-	}
-	fmt.Println(dim)
-	// PrintCave2(cave, dim)
-	var sum int
-	y := 2000000
-	for x := dim[2]; x <= dim[3]; x++ {
-		value, ok := cave[y][x]
-		safe := 0
-		if !ok {
-			// square is empty
-			// check if any sensors are in range
-			safe = InRangeOfSensor(y, x, sens)
-		} else {
-			if value == 3 {
-				safe = 1
-			}
-		}
-		sum += safe
-	}
-	fmt.Println(sum)
-}
-
-func xyz() {
-	lines := ReadFile("input.txt")
-	var dim [4]int
-	cave := make(map[int]map[int]int)
-	for _, line := range lines {
-		ints := ExtractInts(line)
-		for i, j := range ints {
-			switch i % 2 {
-			case 0: //x
-				if j < dim[2] || dim[2] == 0 {
-					dim[2] = j - 10
-				} else if j > dim[3] {
-					dim[3] = j + 10
-				}
-			case 1: //y
-				if j < dim[0] || dim[0] == 0 {
-					dim[0] = j - 10
-				} else if j > dim[1] {
-					dim[1] = j + 10
-				}
-			}
-		}
-	}
-	for i := dim[0]; i <= dim[1]; i++ {
-		cave[i] = make(map[int]int)
-	}
-	for _, line := range lines {
-		ints := ExtractInts(line)
-		var x int
-		var sensor [2]int
-		for i, y := range ints {
-			fmt.Println(i)
-			switch i % 4 {
-			case 0, 2:
-				x = y
-			case 1:
-				cave[y][x] = 3
-				sensor[0] = y
-				sensor[1] = x
-			case 3:
-				cave[y][x] = 4
-				//measure distance
-				d := abs(sensor[0]-y) + abs(sensor[1]-x)
-				for dy := sensor[0] - d; dy <= sensor[0]+d; dy++ {
-					// 1, 3, 5 etc
-					width := d - abs(sensor[1]-dy)
-					for dx := sensor[1] - width; dx <= sensor[1]+width; dx++ {
-						_, ok := cave[dy]
-						if ok {
-							_, ok2 := cave[dy][dx]
-							if !ok2 {
-								cave[dy][dx] = 1
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	fmt.Println(dim)
-	PrintCave2(cave, dim)
-	var sum int
-	for _, c := range cave[2000000] {
-		if c == 1 {
-			sum++
-		}
-	}
-	//fmt.Println(sum)
 }
